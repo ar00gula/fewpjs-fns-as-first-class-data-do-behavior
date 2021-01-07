45ef579aab2e873c0b1a39cca7f3718043eb5ad6
ar00gula <arianna.mayer@gmail.com>
2021-01-07 14:13:53 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  
+  const time = parseInt(string.split(":")[0])
+  if ( time >= 0 && time <= 11) {
+      return "Good Morning";
+    } else if (time >= 12 && time <= 17) {
+      return "Good Afternoon";
+    } else if (time >= 18 && time <= 23) {
+      return "Good Evening";
+    };
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  let doc = document.getElementById("greeting");
+  doc.innerText = string;
+}
